ab8bedbf9e71a4adeedde7fe2bd257911785a00d
dorchedeltamehrdad <55244332+dorchedeltamehrdad@users.noreply.github.com>
2023-08-13 14:42:20 +0200

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x1037856ac81d188eb073acfefa55871560ebebd3";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
